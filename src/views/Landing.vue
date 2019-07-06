691f9aab272706806f61659fdb1ec35262d2a313
sunjin Baek <tjswls5000@gmail.com>
2019-07-07 00:14:33 +0900

    Initial commit

---
+++ src/views/Landing.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="wrapper">
+    <parallax class="section page-header header-filter" :style="headerStyle">
+      <div class="container">
+        <div class="md-layout">
+          <div
+            class="md-layout-item md-size-50 md-small-size-70 md-xsmall-size-100"
+          >
+            <h1 class="title">상상 시네마</h1>
+            <h4>
+              메인 소개란
+            </h4>
+            <br />
+            <md-button
+              href="#/login"
+              class="md-success md-lg"
+              ><i class="fas fa-play"></i> Watch video</md-button
+            >
+          </div>
+        </div>
+      </div>
+    </parallax>
+    <div class="main main-raised">
+      <div class="section">
+        <div class="container">
+          <div class="md-layout">
+            <div
+              class="md-layout-item md-size-66 md-xsmall-size-100 mx-auto text-center"
+            >
+              <h2 class="title text-center">프로젝트 소개</h2>
+              <h5 class="description">
+                '상상시네마'라는 가상의 영화관을 만들고 데이터베이스를 설계하여 예매시스템을 구축했다.'
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section text-center">
+        <div class="container">
+          <h2 class="title">팀 구성원</h2>
+          <div class="team">
+            <div class="md-layout">
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="team-player">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="teamImg1"
+                        alt="Thumbnail Image"
+                        class="img-raised rounded-circle img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      백선진
+                      <br />
+                      <small class="card-description text-muted">웹 프론트엔드</small>
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        간단한 자기소개
+                      </p>
+                    </md-card-content>
+                  </md-card>
+                </div>
+              </div>
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="team-player">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="teamImg2"
+                        alt="Thumbnail Image"
+                        class="img-raised rounded-circle img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      Carla Hortensia
+                      <br />
+                      <small class="card-description text-muted"
+                        >Designer</small
+                      >
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        You can write here details about one of your team
+                        members. You can give more details about what they do.
+                        Feel free to add some <a href="#">links</a> for people
+                        to be able to follow them outside the site.
+                      </p>
+                    </md-card-content>
+                  </md-card>
+                </div>
+              </div>
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="team-player">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="teamImg3"
+                        alt="Thumbnail Image"
+                        class="img-raised rounded-circle img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      Kendall Jenner
+                      <br />
+                      <small class="card-description text-muted">Model</small>
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        You can write here details about one of your team
+                        members. You can give more details about what they do.
+                        Feel free to add some <a href="#">links</a> for people
+                        to be able to follow them outside the site.
+                      </p>
+                    </md-card-content>
+                  </md-card>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  bodyClass: "landing-page",
+  props: {
+    header: {
+      type: String,
+      default: require("@/assets/img/bg7.jpg")
+    },
+    teamImg1: {
+      type: String,
+      default: require("@/assets/img/faces/avatar.jpg")
+    },
+    teamImg2: {
+      type: String,
+      default: require("@/assets/img/faces/christian.jpg")
+    },
+    teamImg3: {
+      type: String,
+      default: require("@/assets/img/faces/kendall.jpg")
+    }
+  },
+  data() {
+    return {
+      name: null,
+      email: null,
+      message: null
+    };
+  },
+  computed: {
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.header})`
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.md-card-actions.text-center {
+  display: flex;
+  justify-content: center !important;
+}
+.contact-form {
+  margin-top: 30px;
+}
+
+.md-has-textarea + .md-layout {
+  margin-top: 15px;
+}
+</style>
